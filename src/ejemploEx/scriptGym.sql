3a012ab86d90b5891d4dfe46a91e99875db65f4f
dam23 <dam23@10.0.2.15>
2026-01-28 23:46:50 +0100

    todo coment y preparado

--- src/ejemploEx/scriptGym.sql
+++ src/ejemploEx/scriptGym.sql
@@ -78,3 +78,49 @@ BEGIN
     WHERE dni = p_dni;
 END;
 $$;
+
+-- Repasemos las consultas de selección
+SELECT * FROM persona;
+-- Selecciones únicas
+SELECT DISTINCT edad FROM personas;
+-- Filtrar
+SELECT * FROM persona WHERE población=’Millanes’;
+SELECT * FROM persona WHERE población=’Millanes’ AND edad > 30;
+SELECT * FROM personas WHERE edad BETWEEN 20 AND 30;
+-- Búsqueda parcial
+SELECT * FROM persona WHERE población LIKE ’%vera%’;
+-- Ordenar
+SELECT * FROM persona ORDER BY nombre ASC;
+-- Limitar el resultado
+SELECT * FROM persona ORDER BY edad ASC LIMIT 5;
+-- Consultas de agregación (SUM, AVG …)
+SELECT COUNT(*) FROM persona;
+Combinar datos, Join
+SELECT nombre FROM propietario
+INNER JOIN veterinario ON propietario.dni = veterinario.dni;
+-- Subconsultas
+SELECT nombre, salario FROM empleado
+WHERE salario = (SELECT MAX(salario) FROM empleado);
+SELECT nombre, salario FROM empleado
+WHERE salario IN (SELECT salario FROM empleados WHERE puesto =
+'Desarrollador');
+-- Con arrays
+SELECT nombre, telefonos[1] AS telefono_principal
+FROM persona;
+SELECT nombre, array_length(telefonos, 1) AS num_telefonos
+FROM persona;
+-- Con datos de tipo compuesto o estructurado
+SELECT nombre
+FROM persona
+WHERE direccion.poblacion = 'Saucedilla';
+SELECT nombre, direccion.calle, telefonos[1] AS primer_telefono
+FROM persona;
+
+--  Procedimiento
+CREATE OR REPLACE PROCEDURE nombre ( IN | OUT | INOUT
+parametro TIPO )
+-- Función
+CREATE OR REPLACE FUNCTION nombre ( IN | OUT | INOUT
+parametro TIPO )RETURNS tipo
+-- Función: SELECT nombre();
+-- Procedimiento: CALL nombre();
